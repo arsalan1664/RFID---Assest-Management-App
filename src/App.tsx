834f4c424dd9e35090010f74cb9195209ee3bc39
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2023-10-11 18:04:44 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar"
+
+
+function App() {
+  return (
+    <div className="h-screen">
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,37 @@
+
+import { UserAvatar } from "./Avatar"
+import { Assest } from "./Assest"
+import { Search } from "./Search"
+
+
+import { ThemMode } from "./ThemeToggle"
+import { Button } from "./ui/button"
+
+
+function NavBar() {
+    return (
+        <div className="fixed h-16  top-0 w-full  border-b">
+            <div className="mx-6 my-3 flex items-center   h-auto w-['85%']">
+                <div className="flex items-center justify-between w-full h-full">
+
+                    <h1 className="text-3xl font-['Poppin'] tracking-widest">RFID Pakistan</h1>
+
+                    <div className="flex gap-2">
+                        <Button variant={"link"}>Dashboard</Button>
+                        <Assest />
+                        <Assest />
+                    </div>
+
+                    <div className="flex gap-3">
+                        <ThemMode />
+                        <Search />
+                        <UserAvatar />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar

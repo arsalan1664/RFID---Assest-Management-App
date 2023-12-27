ebd4ab7da96ddf6bc8143a15d22c44e22781e0ab
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2023-12-27 10:58:42 +0000

    forms ready

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -2,36 +2,49 @@ import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Shell } from "lucide-react";
-import React from "react";
+import React, { FormEvent } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserLogin({}: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
 
-  async function HandleSubmit(event: React.SyntheticEvent) {
+  async function HandleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setIsLoading(true);
 
-    setTimeout(() => {
+    try {
+      // Perform your login logic here
+      console.log({ email, password });
+
+      // Simulate an asynchronous operation (replace with your actual login logic)
+      setTimeout(() => {
+        setIsLoading(false);
+        // Handle successful login or show error messages
+      }, 3000);
+    } catch (error) {
+      console.error("An error occurred during login:", error);
       setIsLoading(false);
-    }, 3000);
+      // Handle errors
+    }
   }
   return (
     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <Card className="bg-accent p-4">
         <CardHeader>
           <CardTitle>
-          {/* AUth containner */}
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Login your account
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
-            </p>
-          </div>
+            {/* AUth containner */}
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Login your account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
+              </p>
+            </div>
           </CardTitle>
         </CardHeader>
 
@@ -49,21 +62,27 @@ export function UserLogin({}: UserAuthFormProps) {
                     autoComplete="email"
                     autoCorrect="off"
                     disabled={isLoading}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
                 <div className="grid gap-1">
                   <Input
-                    id="email"
+                    id="password"
                     placeholder="Passward.."
-                    type="email"
+                    type="password"
                     autoCapitalize="none"
-                    autoComplete="email"
+                    autoComplete="Passward"
                     autoCorrect="off"
                     disabled={isLoading}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
                 <Button disabled={isLoading} className="mt-2">
-                  {isLoading && <Shell className="mr-2  h-4 w-4 animate-spin" />}
+                  {isLoading && (
+                    <Shell className="mr-2  h-4 w-4 animate-spin" />
+                  )}
                   Log In
                 </Button>
 
--- src/components/UserRegisterv3.tsx
+++ src/components/UserRegisterv3.tsx
@@ -3,21 +3,35 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Shell } from "lucide-react";
-import React from "react";
+import React, { FormEvent } from "react";
 import { Card, CardContent, CardHeader } from "./ui/card";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserRegister({}: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [name, setName] = React.useState<string>('');
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
 
-  async function HandleSubmit(event: React.SyntheticEvent) {
+  async function HandleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setIsLoading(true);
 
-    setTimeout(() => {
+    try {
+      // Perform your login logic here
+      console.log({ name, email, password });
+
+      // Simulate an asynchronous operation (replace with your actual login logic)
+      setTimeout(() => {
+        setIsLoading(false);
+        // Handle successful login or show error messages
+      }, 3000);
+    } catch (error) {
+      console.error("An error occurred during login:", error);
       setIsLoading(false);
-    }, 3000);
+      // Handle errors
+    } 
   }
   return (
     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
@@ -50,6 +64,8 @@ export function UserRegister({}: UserAuthFormProps) {
                     autoComplete="email"
                     autoCorrect="off"
                     disabled={isLoading}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                   />
                 </div>
                 <div className="grid gap-1">
@@ -64,6 +80,8 @@ export function UserRegister({}: UserAuthFormProps) {
                     autoComplete="email"
                     autoCorrect="off"
                     disabled={isLoading}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
                 <div className="grid gap-1">
@@ -78,6 +96,8 @@ export function UserRegister({}: UserAuthFormProps) {
                     autoComplete="email"
                     autoCorrect="off"
                     disabled={isLoading}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
 
